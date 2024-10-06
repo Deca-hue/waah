201efcf8a39f96d3ae4e28d9d2651737da21d477
Deca-hue <elitetechservices735@gmail.com>
2024-10-06 13:23:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+     // Submit the form
+    document.getElementById("orderForm").submit();
+ 
+ // JavaScript code to handle form submission
+ document.getElementById("submitButton").addEventListener("click", function() {
+
+
+    // Show an alert when the form is submitted
+    alert("Your coffee order has been submitted. Thank you!");
+});
+
+
